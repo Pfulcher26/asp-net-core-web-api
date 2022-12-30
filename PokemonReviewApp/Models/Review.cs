977e76e8d2784073e4660eafb6475dddee41d82d
pfulcher26 <pfulcher26@gmail.com>
2022-12-30 10:19:59 -0800

    Adds review and reviewer classes to the model.

---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PokemonReviewApp.Models
+{
+	public class Review
+	{
+		public Review()
+		{
+		}
+	}
+}
+
---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PokemonReviewApp.Models
+{
+	public class Reviewer
+	{
+		public Reviewer()
+		{
+		}
+	}
+}
+
