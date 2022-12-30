6a69639643aa79453583fe1d79ebb0d6a1554a9a
pfulcher26 <pfulcher26@gmail.com>
2022-12-30 10:52:38 -0800

    Adds a review property to the pokemon class within the model in order to join the pokemon class to the review class in a one to many relationship. Does the same with Country and Owner, as well as Reviewers and Review calsses.

--- PokemonReviewApp/Models/Country.cs
+++ PokemonReviewApp/Models/Country.cs
@@ -3,9 +3,9 @@ namespace PokemonReviewApp.Models
 {
 	public class Country
 	{
-		public Country()
-		{
-		}
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public ICollection<Owner> Owners{ get; set; }
 	}
 }
 
