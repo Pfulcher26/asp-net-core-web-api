0bcc677bb4a0e7e22d64c291980f2f2888f8a933
pfulcher26 <pfulcher26@gmail.com>
2022-12-30 11:10:25 -0800

    Adds a new class 'Pokemon Category' that forms a many to many relationship between the Pokemon and Category Classes.

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -3,9 +3,11 @@ namespace PokemonReviewApp.Models
 {
 	public class Owner
 	{
-		public Owner()
-		{
-		}
+		public int  Id { get; set; }
+		public string Name { get; set; }
+		public string Gym { get; set; }
+		public Country Country { get; set; }
 	}
 }
 
+ 
